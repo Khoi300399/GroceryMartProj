fd9d08776da29373bd1d86478400b6ab21c86f8c
Thành Khôi <khoimtk321@gmail.com>
2023-07-07 16:20:53 +0700

    add header,hero and services

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,94 @@
-const gulp = require("gulp");
+const { src, task, series, parallel, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
 const pug = require("gulp-pug");
+const plumber = require("gulp-plumber");
 const browserSync = require("browser-sync").create();
 
 // Biên dịch Sass thành CSS
-gulp.task("sass", function () {
-  return gulp
-    .src("./app/**/scss/*.scss")
+task("sassTask", function () {
+  return src("./app/**/scss/*.scss", { sourcemaps: true })
+    .pipe(
+      plumber(function (err) {
+        console.log("Sass Task Error");
+        console.log(err);
+        this.emit("end");
+      })
+    )
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(rename("style.css"))
     .pipe(cssnano())
-    .pipe(gulp.dest("./app/public/css/"));
+    .pipe(dest("./app/public/css/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 });
 
 // Biên dịch Pug thành HTML
-gulp.task("pug", function () {
-  return gulp
-    .src("./app/src/views/*.pug", { sourcemaps: true })
+task("pugTask", function () {
+  return src("./app/src/views/*.pug")
+    .pipe(
+      plumber(function (err) {
+        console.log("Pug Task Error");
+        console.log(err);
+        this.emit("end");
+      })
+    )
     .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest("./app/public/"));
+    .pipe(dest("./app/public/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
+});
+
+// Clone file image
+task("imageTask", function () {
+  return src("./app/src/assets/images/*")
+    .pipe(dest("./app/public/images"))
+    .pipe(
+      plumber(function (err) {
+        console.log("Image Task Error");
+        console.log(err);
+        this.emit("end");
+      })
+    );
 });
 
 // Tạo máy chủ phát triển cục bộ và tự động làm mới trình duyệt
-gulp.task("serve", function () {
+task("browserSyncTask", function (done) {
   browserSync.init({
     server: {
       baseDir: "./app/public",
     },
+    port: 3000,
   });
-  gulp
-    .watch("./app/src/views/**/*.pug", gulp.parallel("pug"))
-    .on("change", browserSync.reload);
-  gulp.watch("./app/src/**/*.scss", gulp.parallel("sass"));
+  done();
 });
 
+function watching() {
+  watch("./app/src/views/**/*.pug", series("pugTask")).on(
+    "change",
+    browserSync.reload
+  );
+  watch("./app/src/**/*.scss", series("sassTask")).on(
+    "change",
+    browserSync.reload
+  );
+  watch("./app/src/assets/images/*", series("imageTask")).on(
+    "change",
+    browserSync.reload
+  );
+}
+
 // Tác vụ xây dựng chính
-gulp.task("build", gulp.series("sass", "pug"));
+task("build", parallel("pugTask", "sassTask", "imageTask"));
 
 // Tác vụ mặc định
-gulp.task("default", gulp.series("build", "serve"));
+task("default", series("build", parallel(watching, "browserSyncTask")));
